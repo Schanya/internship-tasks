67e4251748c8cd4b7206614fa22010978c406dad
Schanya <anyashemelevadm@gmail.com>
2023-10-18 09:27:10 +0300

    decomp, toRange, kthFactor

---
+++ internship/leetcode/medium/theKthFactorOfN.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+const kthFactor = function (n, k) {
+    let factors = [];
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) factors.push(i);
+        if (factors.length === k) return factors[k - 1];
+    }
+    return -1;
+}
