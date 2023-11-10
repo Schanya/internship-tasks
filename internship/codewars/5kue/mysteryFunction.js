a0ce652c4745b321635a573077a12e74dceda92b
Schanya <anyashemelevadm@gmail.com>
2023-11-10 10:54:01 +0300

    alphabetWar, solved, reverseList

---
+++ internship/codewars/5kue/mysteryFunction.js
@@ -0,0 +1,14 @@
+function solved(str) {
+    const strMiddleIndex = Math.floor(str.length / 2);
+
+    if (str.length % 2 === 0) {
+        const strSort = str.split('').sort().join('');
+
+        return strSort;
+    } else {
+        const resultStr = str.split('');
+        const cutStr = resultStr.splice(strMiddleIndex, 1);
+
+        return resultStr.sort().join('');
+    }
+}
